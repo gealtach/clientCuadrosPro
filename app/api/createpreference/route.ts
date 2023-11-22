1ef21ed0898a1b2d39eda944ae80cc7af695d9ad
gealtach <cleivaj93@gmail.com>
2023-11-22 13:04:31 -0300

    we are back

--- app/api/createpreference/route.ts
+++ app/api/createpreference/route.ts
@@ -4,13 +4,13 @@ export async function POST(req: NextRequest) {
     try {
         const cart = await req.json()
         const preferenceInfo = {
-            id: cart.selectedItems[0].url,
+            items:
+            [{id: cart.selectedItems[0].url,
             title: cart.email,
             quantity: 1,
             currency_id: 'CLP',
-            unit_price: cart.totalPrice
+            unit_price: cart.totalPrice}]
         }
-        console.log(preferenceInfo);
         
         const response = await fetch('https://api.mercadopago.com/checkout/preferences',{
             method: "POST",
@@ -21,9 +21,9 @@ export async function POST(req: NextRequest) {
             body: JSON.stringify(preferenceInfo),
         })
         const data = await response.json();
-        console.log(data);
+        console.log(data.init_point);
         
-      return NextResponse.json(data);
+      return NextResponse.redirect(`${data.init_point}`);
     } catch (error) {
       console.error('Error:', error);
       return NextResponse.error();
