5ed00f77f306e6a06b4e6fa91d942e2842457d01
gealtach <cleivaj93@gmail.com>
2023-11-24 13:34:08 -0300

    modificacion de rewrites

--- app/api/createpreference/route.ts
+++ app/api/createpreference/route.ts
@@ -19,19 +19,19 @@ export async function POST(req: NextRequest) {
         const response = await fetch('https://api.mercadopago.com/checkout/preferences',{
             method: "POST",
             headers: {
+              'Authorization': `Bearer ${process.env.acmp}`,
               'Access-Control-Allow-Origin': '*',
               'Access-Control-Allow-Methods': 'POST',
               'Access-Control-Allow-Headers': 'Content-Type, Authorization',
               'Content-Type': 'application/json',
-              'Authorization': `Bearer ${process.env.acmp}`
             },
             body: JSON.stringify(preferenceInfo),
         })
         const data = await response.json();
         console.log(data.init_point);
         
-      //return Response.redirect(`${data.init_point}`);
-      return Response.json(data.init_point);
+      return Response.redirect(`${data.init_point}`);
+      //return Response.json(data.init_point);
 
     } catch (error) {
       console.error('Error:', error);
--- next.config.js
+++ next.config.js
@@ -9,25 +9,22 @@ const nextConfig = {
         acmp: 'TEST-7912064436267766-111617-dd73109e369a818f7c61391e1bca88a6-1551195321',
         pkmp: 'TEST-699c2148-a737-45ac-b65b-af3c34bc4e05'
       },
-      // async rewrites() {
-      //   return [{
-      //     source: '/api/:path*',
-      //     destination: 'https://api.mercadopago.com/:path*'
-      //   }]
-      // }
-      // async headers () {
-      //   return [
-      //     {
-      //       source: '/:path*',
-      //       headers: [
-      //         { key: "Access-Control-Allow-Credentials", value: "true" },
-      //               { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-      //               { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-      //               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-      //       ]
-      //     }
-      //   ]
-      // }
+      async rewrites() {
+        return [
+          {
+            source: '/api/auth/:path*',
+            destination: '/api/auth/:path*',
+          },
+          {
+            source: '/api/:path*',
+            destination: 'https://api.mercadopago.com/:path*',
+          },
+          {
+            source: '/api/:path*',
+            destination: 'https://www.mercadopago.cl/:path*',
+          }
+        ];
+      },
 }
 
 module.exports = nextConfig
